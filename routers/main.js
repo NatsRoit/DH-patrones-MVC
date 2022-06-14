d1116d829244f8fcb9d7efeec598703dd7b6bd9e
NatsRoit <nats.roitman@gmail.com>
2022-06-14 09:55:00 +0200

    package.json

--- routers/main.js
+++ routers/main.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require('path');
+// const path = require('path');
 const mainController = require("../controllers/mainController")
 
 const router = express.Router();
